dbae1390c28bcabde6332d187e4ec00f6aafc8c4
Mayank Raj <mayraj035@gmail.com>
2024-01-11 13:11:47 +0530

    Initial Commit

---
+++ SimpleBarcodeScanner/SimpleBarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBarcodeScannerApp.swift
+//  SimpleBarcodeScanner
+//
+//  Created by Mayank Raj on 2024-01-11.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
